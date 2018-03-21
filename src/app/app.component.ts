d4b4456e5db784bf6eaa75d4c758ce1c07b15741
Divesh Sankhla <sankhlasaini@gmail.com>
2018-03-21 22:13:57 +0530

    Pre Selection fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 // import { Message } from './api/message';
-// import { TreeNode } from './api/treeNode';
+import { TreeNode } from './api/treeNode';
 // import { MenuItem } from './api/menuitem';
 
 
@@ -13,10 +13,27 @@ import { Component, ViewChild, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   filesTree0;
-
+  files;
+  selectedFiles;
   selectedFiles2 = [];
-
+  dataArray = [];
+  dataArray2 = [];
   constructor() { }
+
+  expandAll() {
+    this.filesTree0.forEach(node => {
+      this.expandRecursive(node, true);
+    });
+  }
+
+  private expandRecursive(node, isExpand) {
+    node.expanded = isExpand;
+    if (node.children) {
+      node.children.forEach(childNode => {
+        this.expandRecursive(childNode, isExpand);
+      });
+    }
+  }
 
   public ngOnInit() {
     this.filesTree0 =
@@ -24,7 +41,7 @@ export class AppComponent implements OnInit {
         'levelType': 'state',
         'levelName': 'Karnataka',
         'code': 'KA',
-        'id': 'ChIJj0i_N0xaozsR1Xx10YzS8UE',
+        'id': 'KA',
         'levelCategory': 'STANDARD',
         'isExpandable': true,
         'isEditable': false,
@@ -33,7 +50,7 @@ export class AppComponent implements OnInit {
           'levelType': 'Zone',
           'levelName': 'East',
           'code': null,
-          'id': 'd1a66a53-6bbf-43cb-a822-0c033f8afd1e',
+          'id': 'KA-Ease',
           'levelCategory': 'CUSTOM',
           'isExpandable': false,
           'isEditable': false,
@@ -43,7 +60,7 @@ export class AppComponent implements OnInit {
           'levelType': 'Zone',
           'levelName': 'West',
           'code': null,
-          'id': 'eb58f44e-fd0e-4c99-8ede-1b632093d1f3',
+          'id': 'KA-West',
           'levelCategory': 'CUSTOM',
           'isExpandable': false,
           'isEditable': false,
@@ -52,7 +69,7 @@ export class AppComponent implements OnInit {
             'levelType': 'district',
             'levelName': 'Bangalore Urban',
             'code': null,
-            'id': 'ChIJ_Q7NCkQWrjsRn2yP7O-T8Fg',
+            'id': 'KA-West-BLR',
             'levelCategory': 'STANDARD',
             'isExpandable': true,
             'isEditable': false,
@@ -61,7 +78,7 @@ export class AppComponent implements OnInit {
               'levelType': 'town',
               'levelName': 'Bengaluru',
               'code': null,
-              'id': 'ChIJbU60yXAWrjsR4E9-UejD3_g',
+              'id': 'KA-West-BLR-BLR',
               'levelCategory': 'STANDARD',
               'isExpandable': true,
               'isEditable': false,
@@ -70,7 +87,7 @@ export class AppComponent implements OnInit {
                 'levelType': 'pincode',
                 'levelName': null,
                 'code': null,
-                'id': '7484d0e5-625f-4179-ab94-0057f198e330',
+                'id': 'KA-West-BLR-BLR-PINCODE',
                 'levelCategory': 'STANDARD',
                 'isExpandable': true,
                 'isEditable': false,
@@ -83,7 +100,7 @@ export class AppComponent implements OnInit {
           'levelType': 'Zone',
           'levelName': 'North',
           'code': null,
-          'id': 'af544485-3602-4f0a-b71f-6fd75a4ea83a',
+          'id': 'KA-North',
           'levelCategory': 'CUSTOM',
           'isExpandable': false,
           'isEditable': false,
@@ -93,7 +110,7 @@ export class AppComponent implements OnInit {
           'levelType': 'Zone',
           'levelName': 'South',
           'code': null,
-          'id': '9f4cb32d-224b-4bee-bb84-ef67cb22187a',
+          'id': 'KA-South',
           'levelCategory': 'CUSTOM',
           'isExpandable': false,
           'isEditable': false,
@@ -104,7 +121,7 @@ export class AppComponent implements OnInit {
         'levelType': 'state',
         'levelName': 'Andhra Pradesh',
         'code': 'AP',
-        'id': 'ChIJf9STrvhGNToRg82tlb670TM',
+        'id': 'AP',
         'levelCategory': 'STANDARD',
         'isExpandable': true,
         'isEditable': false,
@@ -113,16 +130,16 @@ export class AppComponent implements OnInit {
           'levelType': 'district',
           'levelName': 'Vishakhapatnam',
           'code': null,
-          'id': 'ChIJP5fmiRNDOToRaIRJlQPC2ZI',
+          'id': 'AP-VISH',
           'levelCategory': 'STANDARD',
           'isExpandable': true,
           'isEditable': false,
           'isSelected': true,
           'subTerritoryLevel': [{
             'levelType': 'town',
-            'levelName': 'Visakhapatnam',
+            'levelName': 'Visakhapatnam Child',
             'code': null,
-            'id': 'ChIJP5fmiRNDOToRaIRJlQPC2ZI',
+            'id': 'AP-VISH_child',
             'levelCategory': 'STANDARD',
             'isExpandable': true,
             'isEditable': false,
@@ -131,7 +148,7 @@ export class AppComponent implements OnInit {
               'levelType': 'pincode',
               'levelName': null,
               'code': null,
-              'id': 'a725092b-17dc-4082-95a6-244e112e0f77',
+              'id': 'AP-VISH-cjild-PINCODE',
               'levelCategory': 'STANDARD',
               'isExpandable': true,
               'isEditable': false,
@@ -141,7 +158,7 @@ export class AppComponent implements OnInit {
               'levelType': 'pincode',
               'levelName': null,
               'code': null,
-              'id': 'c1463119-ec9f-47d5-9a7e-1d031fa09acd',
+              'id': 'AP-VISH-cjild-PINCODE-2',
               'levelCategory': 'STANDARD',
               'isExpandable': true,
               'isEditable': false,
@@ -153,7 +170,7 @@ export class AppComponent implements OnInit {
           'levelType': 'district',
           'levelName': 'Anantapuram',
           'code': null,
-          'id': 'ChIJ7UI4G8ZKsTsRi2DdoCb6SF4',
+          'id': 'AP-ANA',
           'levelCategory': 'STANDARD',
           'isExpandable': true,
           'isEditable': false,
@@ -162,11 +179,141 @@ export class AppComponent implements OnInit {
         }]
       }];
     this.modifyData(this.filesTree0);
+
+
+    this.files =
+      [
+        {
+          "label": "Documents",
+          "data": "Documents Folder",
+          "expandedIcon": "fa-folder-open",
+          "collapsedIcon": "fa-folder",
+          "leaf": false,
+          "children": [{
+            "label": "Work",
+            "data": "Work Folder",
+            "expandedIcon": "fa-folder-open",
+            "collapsedIcon": "fa-folder",
+            "leaf": false,
+            "children": [{ "label": "Expenses.doc", "icon": "fa-file-word-o", "data": "Expenses Document", "leaf": true }, { "label": "Resume.doc", "icon": "fa-file-word-o", "data": "Resume Document", "leaf": true }]
+          },
+          {
+            "label": "Home",
+            "data": "Home Folder",
+            "expandedIcon": "fa-folder-open",
+            "collapsedIcon": "fa-folder",
+            "leaf": false,
+            "children": [{ "label": "Invoices.txt", "icon": "fa-file-word-o", "data": "Invoices for this month", "leaf": true }]
+          }]
+        },
+        {
+          "label": "Pictures",
+          "data": "Pictures Folder",
+          "expandedIcon": "fa-folder-open",
+          "collapsedIcon": "fa-folder",
+          "leaf": false,
+          "children": [
+            { "label": "barcelona.jpg", "icon": "fa-file-image-o", "data": "Barcelona Photo", "leaf": true },
+            { "label": "logo.jpg", "icon": "fa-file-image-o", "data": "PrimeFaces Logo", "leaf": true },
+            { "label": "primeui.png", "icon": "fa-file-image-o", "data": "PrimeUI Logo", "leaf": true }]
+        },
+        {
+          "label": "Movies",
+          "data": "Movies Folder",
+          "expandedIcon": "fa-folder-open",
+          "collapsedIcon": "fa-folder",
+          "leaf": false,
+          "children": [{
+            "label": "Al Pacino",
+            "data": "Pacino Movies",
+            "leaf": false,
+            "children": [{ "label": "Scarface", "icon": "fa-file-video-o", "data": "Scarface Movie", "leaf": true }, { "label": "Serpico", "icon": "fa-file-video-o", "data": "Serpico Movie", "leaf": true }]
+          },
+          {
+            "label": "Robert De Niro",
+            "data": "De Niro Movies",
+            "leaf": false,
+            "children": [{ "label": "Goodfellas", "icon": "fa-file-video-o", "data": "Goodfellas Movie", "leaf": true }, { "label": "Untouchables", "icon": "fa-file-video-o", "data": "Untouchables Movie", "leaf": true }]
+          }]
+        }
+      ];
+    this.dataArray = ["Expenses Document", "Invoices for this month", "PrimeFaces Logo", "Untouchables Movie", "Goodfellas Movie"];
+    this.dataArray2 = [
+      "AP-ANA",
+      "KA-West-BLR-BLR-PINCODE",
+      "KA-Ease",
+      "KA-North",
+      "KA-South",
+      "AP-VISH-cjild-PINCODE-2",
+      // "AP-VISH_child"
+    ]
+    this.selectedFiles = [];
+    // this.checkNode(this.files, this.dataArray);
+    this.checkNode(this.filesTree0, this.dataArray2);
+    console.log(this.filesTree0);
+    console.log(this.selectedFiles);
+
+    this.expandAll();
+
   }
 
+  checkNode(nodes, str) {
+    this.addLeafFlag(nodes);
+    for (let i = 0; i < nodes.length; i++) {
+      if (str.indexOf(nodes[i].id) > -1) {
+        if (this.selectedFiles.indexOf(nodes[i]) === -1) {
+          this.selectedFiles.push(nodes[i]);
+        }
+      }
+      if (!nodes[i].leaf) {
+        for (let j = 0; j < nodes[i].children.length; j++) {
+          console.log(nodes[i].children[j].id);
+          if (str.find((st) => st === nodes[i].children[j].id)) {
+            if (!this.selectedFiles.find((f) => f.id === nodes[i].children[j].id)) {
+              this.selectedFiles.push(nodes[i].children[j]);
+            }
+          }
+        }
+
+        this.checkNode(nodes[i].children, str);
+
+      }
+      let count = nodes[i].children.length;
+      let c = 0;
+      for (let j = 0; j < nodes[i].children.length; j++) {
+        if (this.selectedFiles.includes(nodes[i].children[j])) {
+          c++;
+        }
+        if (nodes[i].children[j].partialSelected) nodes[i].partialSelected = true;
+      }
+      if (c == 0) { }
+      else if (c == count) {
+        nodes[i].partialSelected = false;
+        if (!this.selectedFiles.includes(nodes[i])) {
+          this.selectedFiles.push(nodes[i]);
+        }
+      }
+      else {
+        nodes[i].partialSelected = true;
+      }
+    }
+  }
+
+
+  addLeafFlag(nodes) {
+    nodes.forEach(node => {
+      if (node.children.length > 0) {
+        node.leaf = false;
+        this.addLeafFlag(node.children);
+      } else {
+        // console.log(node.id)
+        node.leaf = true;
+      }
+    });
+  }
   modifyData(data) {
     data.forEach(element => {
-      element.label = element.levelName;
+      element.label = element.id;
       element.children = element.subTerritoryLevel;
       delete element.subTerritoryLevel;
       if (element.children.length > 0) {
@@ -176,8 +323,8 @@ export class AppComponent implements OnInit {
   }
 
   nodeSelect(event) {
-    console.log(this.selectedFiles2);
-    event.node.isSelected = true;
+    console.log(this.selectedFiles);
+    // event.node.isSelected = true;
   }
 
   nodeUnselect(event) {
